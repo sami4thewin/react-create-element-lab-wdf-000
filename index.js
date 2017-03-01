4e6ca5163f0ba8a6cdc118ed9f519cfc28bf68c2
sami4thewin <samiomarmohammed@gmail.com>
2017-03-01 08:02:35 -0500

    Working done

--- index.js
+++ index.js
@@ -5,7 +5,12 @@ const meInReact =
       React.createElement('h1', {}, 'An Awesome Person'),
       React.createElement('p', {}, 'Who is learning React'),
       React.createElement('ul', {className: 'me__interests'},
-        
+        [
+          React.createElement('li', {}, 'JavaScript'),
+          React.createElement('li', {}, 'React'),
+          React.createElement('li', {}, 'Movies'),
+          React.createElement('li', {}, 'Ice cream')
+        ]
       )
     ]
 );
